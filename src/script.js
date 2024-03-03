2967d39edcf880854fcf410a8578ab596794a2ab
din0x <superrobertxd52@gmail.com>
2024-03-03 14:31:41 +0100

    Improve background

--- src/script.js
+++ src/script.js
@@ -1,102 +1,130 @@
 const fromHTML = (html) => {
-	const helper = document.createElement("div")
-	helper.innerHTML += html
-	return helper.lastChild
-}
+	const helper = document.createElement("div");
+	helper.innerHTML += html;
+	return helper.lastChild;
+};
 
 function setupGreetings() {
-	const message = "Greetings, I am Robert"
-	const speed = 150
-	let greetings = document.getElementById("greetings")
+	const message = "Greetings, I am Robert";
+	const speed = 150;
+	let greetings = document.getElementById("greetings");
 	if (greetings == null) {
-		console.warn("Couldn't find greetings element.")
-		return
+		console.warn("Couldn't find greetings element.");
+		return;
 	}
 
-	let progress = 0
+	let progress = 0;
 
-	let interval = setInterval(() => {
-		greetings.textContent = "> " + message.slice(0, progress)
-		progress++
+	const interval = setInterval(() => {
+		greetings.textContent = "> " + message.slice(0, progress);
+		progress++;
 
 		if (progress > greetings.length) {
-			clearInterval(interval)
+			clearInterval(interval);
 		}
-	}, speed)
+	}, speed);
 }
 
 function setupBackground() {
-	const maxCount = 100
-	const autoDestroyTime = 10
-	const hueStep = 0.35
-	let background = document.getElementById("background")
+	const maxCount = 100;
+	const autoDestroyTime = 25;
+	const hueStep = 0.35;
+	const width = 10;
+
+	const background = document.getElementById("background");
 	if (background == null) {
-		console.warn("Couldn't find background.")
-		return
+		console.warn("Couldn't find background.");
+		return;
 	}
 
-	let hue = 0
+	let hue = 0;
+
+	/**
+	 * @param {number} x1
+	 * @param {number} y1
+	 * @param {number} x2
+	 * @param {number} y2
+	 */
+	function drawLine(x1, y1, x2, y2, color) {
+		x1 -= width / 2;
+		y1 -= width / 2;
+		x2 -= width / 2;
+		y2 -= width / 2;
+
+		const length = Math.sqrt(Math.abs((x1 - x2) * (x1 - x2)) + Math.abs((y1 - y2) * (y1 - y2)));
+
+		const left = x1 - (x1 - x2) / 2 - length / 2;
+		const top = y1 - (y1 - y2) / 2;
+		const rotate = -Math.atan2(x1 - x2, y1 - y2) + Math.PI / 2;
+
+		background.appendChild(fromHTML(`<div style="position: absolute; display: inline-block; left: ${left}px; top: ${top}px; width: ${length + 5}px; height: ${width}px; transform: rotate(${rotate}rad); border-radius: 100%; background-color: ${color}"></div>`));
+	}
 
 	setInterval(() => {
 		if (background.childElementCount <= 0) {
-			return
+			return;
 		}
 
-		background.removeChild(background.firstChild)
-	}, autoDestroyTime)
+		background.removeChild(background.firstChild);
+	}, autoDestroyTime);
+
+	let lastX = null;
+	let lastY = null;
 
 	window.addEventListener("mousemove", (event) => {
-		const top = event.clientY + document.documentElement.scrollTop - 15
-		const left = event.clientX - 15
+		const top = event.clientY + document.documentElement.scrollTop;
+		const left = event.clientX;
 
-		// let color = `hsla(${hue}, 35%, 50%, 0.125)`
-		let color = `hsla(103, 35%, 56%, 0.125)`
-		let point = fromHTML(`<div class="point" style="left: ${left}px; top: ${top}px; background-color: ${color}"></div>`)
+		let color = `hsla(${hue}, 35%, 50%, 0.125)`;
+		// let color = `hsla(103, 35%, 56%, 0.125)`
 
-		background.appendChild(point)
+		drawLine(lastX, lastY, left, top, color);
 
-		hue += hueStep
-		hue %= 360
+		lastX = left;
+		lastY = top;
+
+		hue += hueStep;
+		hue %= 360;
 
 		if (background.childElementCount > maxCount) {
-			background.removeChild(background.firstChild)
+			background.removeChild(background.firstChild);
 		}
-	})
+	});
 }
 
 function setupFadeIn() {
-	const speed = 75
-	const start = 20
-	const step = 2
-	let top = start
-	let opacity = 0
+	const speed = 75;
+	const start = 20;
+	const step = 2;
+	let top = start;
+	let opacity = 0;
 
-	let elements = document.getElementsByClassName("fade-in")
+	let elements = document.getElementsByClassName("fade-in");
 
 	for (let i = 0; i < elements.length; i++) {
-		elements[i].style.top = `${top * (i + 1)}px`
-		elements[i].style.opacity = `${opacity}`
+		elements[i].style.top = `${top * (i + 1)}px`;
+		elements[i].style.opacity = `${opacity}`;
 	}
 
 	let interval = setInterval(() => {
-		top -= step
+		top -= step;
 
 		if (opacity < 100) {
-			opacity += step / start
+			opacity += step / start;
 		}
 		if (top <= 0) {
-			clearInterval(interval)
+			clearInterval(interval);
 		}
 
 		for (let i = 0; i < elements.length; i++) {
-			elements[i].style.opacity = `${opacity}`
-			elements[i].style.top = `${top * (i + 1)}px`
+			elements[i].style.opacity = `${opacity}`;
+			elements[i].style.top = `${top * (i + 1)}px`;
 		}
-	}, speed)
+	}, speed);
 }
 
 window.onload = () => {
-	setupGreetings()
-	setupBackground()
-	setupFadeIn()
-}
+	setupGreetings();
+	setupBackground();
+	setupFadeIn();
+};
